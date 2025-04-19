3cc6f48dcb39c1758d701d03b2c4bb9f6810cf2d
Grecco, Franco <srgrecco93@gmail.com>
2025-04-19 20:31:44 +0200

    finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,84 @@
-import Image from "next/image";
+'use client'
+
+import { cn } from '@/lib/utils';
+import { Montserrat, Fraunces } from 'next/font/google'
+import { useEffect, useState } from 'react';
+
+const montserrat = Montserrat()
+const fraunces = Fraunces()
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const targetBreakpoint = "(max-width: 640px)";
+  const [isMobile, setIsMobile] = useState(window?.matchMedia(targetBreakpoint)?.matches);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  useEffect(() => {
+    const event = window.matchMedia(targetBreakpoint);
+    event.onchange = (e) => setIsMobile(e.matches);
+
+    return () => {
+      event.onchange = null;
+    }
+  }, []);
+
+  return (
+    <>
+      <main className="h-screen grid grid-cols-1 place-content-center">
+        <div className="col-span-full grid grid-cols-1 sm:grid-cols-2 sm:max-w-lg w-8/10 m-auto bg-white shadow-sm rounded-lg">
+          <div>
+            <img
+              src={ isMobile ? '/image-mobile.jpg' : '/image-desktop.jpg' } 
+              alt="Gabrielle Esence Eau de Parfum"
+              className='size-full object-cover rounded-t-lg sm:rounded-l-lg sm:rounded-r-none'
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div className="p-5 flex flex-col justify-center gap-2.5 sm:gap-0 sm:justify-around">
+            <h2 className={cn(
+              'text-xs font-mono tracking-widest text-foreground-dimmed',
+              montserrat.className,
+              )}
+            >
+              PERFUME
+            </h2>
+            <h1 className={cn(
+              'text-2xl font-bold sm:text-3xl',
+              fraunces.className,
+            )}>
+              Gabrielle Essence Eau De Parfum
+            </h1>
+            <p className='text-xs text-pretty text-foreground-dimmed'>
+              A floral, solar and voluptuous interpretation composed by Olivier Polge,
+              Perfumer-Creator for the House of CHANEL.
+            </p>
+            <div className='flex items-center gap-4'>
+              <span className={cn(
+                fraunces.className,
+                'text-2xl font-bold text-primary'
+              )}>
+                $149.99
+              </span>
+              <span className='text-xs line-through text-foreground-dimmed'>
+                $169.99
+              </span>
+            </div>
+            <button className={cn(
+                'flex justify-center items-center gap-2 p-3 rounded-md shadow-sm',
+                'text-xs text-primary-foreground bg-primary',
+                'hover:bg-primary-accent focus:bg-primary-accent',
+                'hover:cursor-pointer'
+              )}
+            >
+              <img src='/icon-cart.svg'/>
+              <div className='font-extrabold'>Add to Cart</div>
+            </button>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      {/* <footer className="flex-none">
+        <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+          Coded by Grecco Oliva, Franco <a href="#">(Github)</a>.
+        </div>
+      </footer> */}
+    </>
   );
 }
